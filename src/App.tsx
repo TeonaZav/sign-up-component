31cf2572aa6b4531d1465a06cb231e3c8abb968b
TeonaZav <teona.zavrashvili@gmail.com>
2023-01-10 02:08:46 +0400

    add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import SignUpForm from "./components/SignUpForm";
-
+import HeroSection from "./components/SectionHero";
 function App() {
   return (
     <div className="App">
+      <HeroSection />
       <SignUpForm />
     </div>
   );
---
+++ src/components/SectionHero.jsx
@@ -0,0 +1,15 @@
+const HeroSection = () => {
+  return (
+    <section>
+      <div>
+        <h1>Learn to code by watching others</h1>
+        <p>
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
+        </p>
+      </div>
+    </section>
+  );
+};
+export default HeroSection;
