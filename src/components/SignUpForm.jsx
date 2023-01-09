ebfec2a48e68c9c9c6dc8067c9a3f06fd8c3e1c8
TeonaZav <teona.zavrashvili@gmail.com>
2023-01-10 01:21:49 +0400

    feat: add sign up form

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { Button, VStack } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import TextField from "./TextField";
+const SignUpForm = () => {
+  const [errorM, setErrorM] = useState(null);
+  return (
+    <div className="form-main-ct">
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        }}
+        onSubmit={(values, actions) => {
+          const vals = { ...values };
+          actions.resetForm();
+          fetch(`${process.env.REACT_APP_SERVER_URL}/auth/register`, {
+            method: "POST",
+            credentials: "include",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(vals),
+          })
+            .catch((err) => {
+              return;
+            })
+            .then((res) => {
+              if (!res || !res.ok || res.status >= 400) {
+                return;
+              }
+              return res.json();
+            })
+            .then((data) => {
+              if (!data) return;
+
+              if (data.status) {
+                setErrorM(data.status);
+              } else if (data.loggedIn) {
+              }
+            });
+        }}
+      >
+        <VStack as={Form} className="form-ct">
+          <TextField
+            name="firstName"
+            placeholder="First Name"
+            autoComplete="off"
+            type="text"
+          />
+          <TextField
+            name="LastName"
+            placeholder="Last Name"
+            autoComplete="off"
+            type="text"
+          />
+          <TextField
+            name="email"
+            placeholder="Email address"
+            autoComplete="off"
+          />
+          <TextField
+            name="password"
+            placeholder="Password"
+            autoComplete="off"
+            type="password"
+          />
+
+          <Button colorScheme="teal" type="submit" className="btn-sign-up">
+            Sign Up
+          </Button>
+        </VStack>
+      </Formik>
+    </div>
+  );
+};
+export default SignUpForm;
